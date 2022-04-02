fc999ad2143959ff0a4376407bdf5a16174b1157
zhengshuai <zhengshuai@honganrobots.com>
2022-04-02 16:16:23 +0800

    upd: 第一天

--- vue_ts/src/router/index.ts
+++ vue_ts/src/router/index.ts
@@ -13,13 +13,25 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import('../views/primary.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/main',
+    name: 'main',
+    component: () => import('../views/main.vue'),
+    children: [
+      {
+        path: '/about',
+        name: 'about',
+        component: () => import('../views/AboutView.vue')
+      }
+    ]
+  },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
 ]
 
 const router = createRouter({
---
+++ vue_ts/src/views/main.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="main">
+		<el-container>
+      <el-header class="header">你渴望知识吗</el-header>
+      <el-container style="height: 92vh">
+        <el-aside class="aside" width="200px">
+					<h5>菜单</h5>
+					<el-menu
+						:default-active="router.path"
+						class="el-menu-vertical-demo"
+						@open="handleOpen"
+						@close="handleClose"
+						router
+					>
+						<el-menu-item v-for="item in pathArr" :key="item.pathId" :index="item.path">
+							<span>{{item.name}}</span>
+						</el-menu-item>
+					</el-menu>
+				</el-aside>
+        <el-main class="cont">
+					<router-view></router-view>
+				</el-main>
+      </el-container>
+    </el-container>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "@vue/runtime-core";
+import { useRouter } from "vue-router";
+export default defineComponent({
+  setup() {
+		const router = useRouter()
+		const pathArr = ref([
+			{pathId: 0, name: '关于', path: '/About'}
+		])
+		return {
+			router,
+			pathArr
+		}
+	},
+});
+</script>
+
+<style scoped lang="less">
+	.main{
+		width: 100%;
+		height: 100%;
+		.header{
+			background: #ccc;
+			height: 8vh;
+			line-height: 8vh;
+		}
+		.aside{
+			border: 1px solid #1c1c1c;
+			// background: #1d1e23;
+			color: #828284;
+		}
+		.cont{
+			background: #ccc;
+			border: 1px solid #1c1c1c;
+		}
+	}
+</style>
