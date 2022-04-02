ef91a26b5bed4e308e97d7294cb0d37477ad8da8
zhengshuai <zhengshuai@honganrobots.com>
2022-04-02 18:00:18 +0800

    upd: 日常迭代

---
+++ vue_ts/src/components/Menu/Menu.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>123</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue-demi";
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  setup() {
+    const R = useRouter();
+    return {
+      R,
+    };
+  },
+});
+</script>
+
+<style lang="less" scoped>
+</style>
